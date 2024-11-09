74435490e2b3750a9c68bbd217c15e16f26f50aa
datnt <datntdev02@gmail.com>
2024-11-09 19:41:17 +0700

    handle change config

--- src/util/axiosCustomize/axiosConfig.js
+++ src/util/axiosCustomize/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://35.247.160.131/api/",
+  // baseURL: "http://35.247.160.131/api/",
+  baseURL: "https://fams-management.tech/api/",
 });
 
 instance.interceptors.request.use(
